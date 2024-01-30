f3f3b38e4d60433a8c6022f9c1dd6e226d7d6c08
HAyleyg21 <143545391+HAyleyg21@users.noreply.github.com>
2024-01-30 16:00:45 -0500

    Add files via upload

---
+++ Ex1_Output.py
@@ -0,0 +1,17 @@
+# Chap2 - Variables
+# Using print() function to display
+
+# The print function can display the result of evaluated expressions.
+print('Hello \nWorld')
+print("Hello Python World")
+
+# The print function always display in a newline.
+print("Hello")
+print("I Can't wait to graduate....;)")
+print("World")
+
+print(123)
+print (1+2+3)
+
+# A single print function can display several values.
+print (3+2, 3-2, 3*2)
