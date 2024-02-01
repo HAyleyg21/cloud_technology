725c86272718228c797bc32e51fdbb366cd23f68
HAyleyg21 <143545391+HAyleyg21@users.noreply.github.com>
2024-02-01 15:30:50 -0500

    Add files via upload

--- Ex1_Output.py
+++ Ex1_Output.py
@@ -5,6 +5,7 @@
 print('Hello \nWorld')
 print("Hello Python World")
 
+
 # The print function always display in a newline.
 print("Hello")
 print("I Can't wait to graduate....;)")
@@ -15,3 +16,15 @@ print (1+2+3)
 
 # A single print function can display several values.
 print (3+2, 3-2, 3*2)
+
+
+# The print function can display the result of evaluated expressions.
+print('Hayley\nGrant')
+print("Hello Python World")
+
+print('\nItem'       '\t Price' )      
+print('\nApple'      '\t $1.75        )     
+print('\nBanana'     '\t          )     
+print('\nCherry'     '\t           )     
+print('\nTotal'      '\t          )      
+
