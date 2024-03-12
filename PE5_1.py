deabb5d1eb73c543179a5b2260befa0d295272d0
HAyleyg21 <143545391+HAyleyg21@users.noreply.github.com>
2024-03-12 15:56:15 -0400

    Add files via upload

--- PE5_1.py
+++ PE5_1.py
@@ -1,1 +1,22 @@
+#A
+a = list(range(5))
+print(a)
+#B
+b = []
+for i in range (5):
+ b.append(i)
+print(b)
+#C
+x = list(range(-10, 10))
+print(x)
+print(min(x), max(x), sum(x))
+#D
+even_num = list(range(2, 11, 2))
+print(even_num[0], even_num[-1])
+#E #Print all the odd numbers from 1 to 9 inclusive in a list, odd_num.
+oddnum = [value for value in range(1,8)]
+print (oddnum)
+#F Make a list of the first 10 cubes and use a for loop to print out the value of each cube in a new line (see output below).
+
+#G #Use a list comprehension to generate a list of the first 10 cubes.Use a for loop to print out the value of each cube in a rowseparated by a ‘|’ (see output below).
 
