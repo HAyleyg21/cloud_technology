fb508bdc777cc66bc72282e1e7f3007fee605c91
HAyleyg21 <143545391+HAyleyg21@users.noreply.github.com>
2024-04-02 16:00:01 -0400

    Add files via upload

---
+++ PE6_3.py
@@ -0,0 +1,5 @@
+#a) Make a list of five usernnames, including the name “admin”.
+#b) Loop through the list and print a greeting to each user.
+#c) If the username is “admin”, print a special greeting, such as "Hello Admin, would you like to see a status report?""
+#d) Otherwise, print a generic greeting, such as
+#e) Implement if the list is empty by printing the message, We need to find some users.
